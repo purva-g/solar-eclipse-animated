462bf7fd71f631ca1b3c9bbe5d524449409a99bc
PURVA <126910520+purva-g@users.noreply.github.com>
2023-03-07 14:38:49 +0530

    Update solar.js

--- solar.js
+++ solar.js
@@ -1,8 +1,11 @@
+//create canvas
 function setup() {
   createCanvas(400, 400);
 }
+//create variable for moon and background
 var moon = -43;
 var back = 0;
+
 function draw() {
     noStroke();
   background(25, 25, 25);
@@ -10,6 +13,7 @@ function draw() {
   ellipse(200,200,200,200);
   fill(31, 29, 29);
 ellipse(moon,200,198,198);
+  //movement of moon
 moon+=0.2;
 back-=0;
 }
